b5f12c26be355693ae6ac43068d5753ba37f26cb
pranab56 <pronabhalder53@gmail.com>
2022-03-25 23:40:48 +0600

    heder

---
+++ src/component/Heder/Heder.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Heder.css'
+const Heder = () => {
+    return (
+        <div className='heder'>
+            <h1>Take any phone</h1>
+        </div>
+    );
+};
+
+export default Heder;
---
+++ src/component/Heder/Shop/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <nav>
+            <div>
+            <h1>my phone</h1>
+        </div>
+        <div>
+            <h3>phone ditals </h3>
+        </div>
+        </nav>
+    );
+};
+
+export default Shop;
